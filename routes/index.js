70efc60e88f5bfbb61e44609b2c9bc8ab00ea7b4
stnor <stefan@selessia.com>
2024-03-24 16:29:29 +0100

    min max tid

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,23 @@ const db = new sqlite3.Database('./db/temperature.db');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    db.all("SELECT * FROM TEMPERATURE ORDER BY ID DESC limit " + SENSOR_COUNT, (err, rows) => {
-        console.log(err, rows);
+    const last24hours = new Date().getTime() - 60 * 60 * 24 * 1000;
+    db.all("SELECT * FROM TEMPERATURE WHERE obstime > " + last24hours + " ORDER BY ID DESC", (err, rows) => {
         rows.forEach(row => {
             row.sensorName = sensorFriendlyNames.get(row.sensor);
-            row.temperatureC = (row.temperature / 100).toFixed(1);
-            row.obstime = new Date(row.obstime).toLocaleString('sv-SE');
+            row.temperatureC = Number((row.temperature / 100).toFixed(1));
         });
-        res.render('index', {tempSensors: rows});
+        const observationTimes = [...new Set(rows.map(r => r.obstime))];
+        let lowestPerObs = [];
+        for (let obs of observationTimes) {
+            const tempsInObs = rows.filter(r => r.obstime == obs).map(r=>r.temperatureC);
+            //plocka ur det lägsta värdet per observation
+            lowestPerObs.push(Math.min(...tempsInObs));
+        }
+        res.render('index', {tempSensors: rows.slice(0, SENSOR_COUNT),
+                                obsTime: new Date(rows[0].obstime).toLocaleString('sv-SE'),
+                                max: Math.max(...lowestPerObs),
+                                min: Math.min(...lowestPerObs)});
     });
 });
 module.exports = router;
